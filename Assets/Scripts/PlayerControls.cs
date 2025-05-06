937dab3355dfb73bf75e565b86b005e026e92052
Ember Willow Jones <st10540923@vgconnect.edu.za>
2025-05-06 14:11:14 +0200

    Adding FallMultiplier to jump to make it more snappy

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -12,7 +12,7 @@ public class PlayerControls : MonoBehaviour
     float SpeedMultiplier=1;
     public float VeerSpeed = 2;
     public bool SpeedBoosted = false;
-
+    public float FallMultiplier=2f;
     GameEngine gameEngine;
     public float JumpForce = 400f;
     [Serialize] public LayerMask GroundMask;
@@ -24,9 +24,18 @@ public class PlayerControls : MonoBehaviour
     private void FixedUpdate()
     {
         if (Dead) return;
+
+        //Movement Controlls
         Vector3 Run = transform.forward * ForwardSpeed  *SpeedMultiplier* Time.fixedDeltaTime;
         Vector3 Veer = transform.right * VeerInput*SpeedMultiplier* Time.fixedDeltaTime;
         rb.MovePosition(rb.position + Run + Veer);
+
+        //Falling Modifier
+
+        if (rb.angularVelocity.y<0)
+        {
+            rb.angularVelocity += Vector3.up * Physics.gravity.y * FallMultiplier * Time.deltaTime;
+        }
     }
     private void Update()
     {
